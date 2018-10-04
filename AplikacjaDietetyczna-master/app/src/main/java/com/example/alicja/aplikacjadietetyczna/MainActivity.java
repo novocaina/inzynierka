7e50fad3fb40fd636a316539270fc08b16cced16
alicja <ali74cja96@o2.pl>
2018-10-04 22:36:49 +0200

    add drawer

--- AplikacjaDietetyczna-master/app/src/main/java/com/example/alicja/aplikacjadietetyczna/MainActivity.java
+++ AplikacjaDietetyczna-master/app/src/main/java/com/example/alicja/aplikacjadietetyczna/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.alicja.aplikacjadietetyczna;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     GridLayout mainGrid;
-
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    drawerLayout=(DrawerLayout)findViewById(R.id.drawer_main);
+    toggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_close);
+    drawerLayout.addDrawerListener(toggle);
+    toggle.syncState();
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mainGrid=findViewById(R.id.mainGrid);
         setSingleEvent(mainGrid);
     }
@@ -41,4 +51,26 @@ public class MainActivity extends AppCompatActivity {
             });
         }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        switch(id){
+case R.id.nav_list:
+    startActivity(new Intent(MainActivity.this,CateringActivity.class));
+    drawerLayout.closeDrawers();
+    return true;
+            case R.id.nav_map:
+                startActivity(new Intent(MainActivity.this,CateringActivity.class));
+                return true;}
+return false;
+}
 }
---
+++ AplikacjaDietetyczna-master/app/src/main/java/com/example/alicja/aplikacjadietetyczna/PagerAdapter.java
@@ -0,0 +1,31 @@
+package com.example.alicja.aplikacjadietetyczna;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter{
+    int pageNumber;
+    public PagerAdapter(FragmentManager fm, int number){
+        super(fm);
+        this.pageNumber=number;
+    }
+    @Override
+    public Fragment getItem(int position) {
+switch (position){
+    case 0:
+        BMIFragment bmiFragment=new BMIFragment();
+        return  bmiFragment;
+    case 1:
+        CPMFragment cpmFragment=new CPMFragment();
+        return  cpmFragment;
+}
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return pageNumber;
+    }
+}
